17c7adf9277770d4324dda4d6961bcbc07c3de6f
AlexDecker <alexanderdecker@dcc.ufmg.br>
2018-04-25 16:04:47 -0300

    mininet script with raw input

--- mininet/1sw_topo.py
+++ mininet/1sw_topo.py
@@ -9,22 +9,22 @@ from eBPFSwitch import eBPFSwitch, eBPFHost
 from time import sleep
 
 class SingleSwitchTopo(Topo):
-    def __init__(self, **opts):
+    def __init__(self, i,**opts):
         # Initialize topology and default options
         Topo.__init__(self, **opts)
 
         switch = self.addSwitch('s1',
             switch_path="../inputqueuedswitch/inputqueuedswitch")
 
-        for h in xrange(2): #TODO number of hosts
+        for h in xrange(i): #TODO number of hosts
             host = self.addHost('h%d' % (h + 1),
                                 ip = "10.0.%d.10/24" % h,
                                 mac = '00:04:00:00:00:%02x' %h)
 
             self.addLink(host, switch)
 
-def main():
-    topo = SingleSwitchTopo()
+def main(i):
+    topo = SingleSwitchTopo(i)
     net = Mininet(topo = topo, host = eBPFHost, switch = eBPFSwitch, controller = None)
 
     net.start()
@@ -32,4 +32,5 @@ def main():
     net.stop()
 
 if __name__ == '__main__':
-    main()
+    i = int(input('Number of hosts: '))
+    main(i)
